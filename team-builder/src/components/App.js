6412ef913ceece2f447b5809a3461bc27254599f
Priscila Ribeiro Monteiro <pririmonteiro@Priscilas-MBP.attlocal.net>
2021-08-17 18:35:27 -0700

    completed team-builder

--- team-builder/src/components/App.js
+++ team-builder/src/components/App.js
@@ -1,82 +1,82 @@
 import '../App.css';
-import React, { useState, useEffect } from 'react';
-import Team from './Team';
-import TeamForm from './TeamForm';
-import axios from '../axios';
+import React, { useState } from 'react'
 
-// 👉 the shape of the state that drives the form
-const initialFormValues = {
-  ///// TEXT INPUTS /////
-  name: '',
-  email: '',
-  ///// DROPDOWN /////
-  role: '',
-}
+const Team = [
+  { Name: 'Austen Allred', Role: 'Founder & CEO', email: 'austen@lambda.com' },
+  { Name: 'Chris Atoki', Role: 'Instructor', email: 'chris@lambda.com' },
+  { Name: 'Brit hemming', Role: 'Instructor', email: 'brit@lambda.com' },
+  { Name: 'Priscila Monteiro', Role: 'Student', email: 'priscila@lambda.com' }
 
+]
+const initialFormValues = { Name: '', Role: '', email: '' }
 
-export default function App() {
-  const [team, setTeam] = useState(initialFormValues);
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const [error, setError] = useState('');
+function App() {
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [team, setTeam] = useState(Team)
 
-  const updateForm = (inputName, inputValue) => {
-    setFormValues({...setFormValues, [inputName]: inputValue});
+  const change =(event) => {
+    const { value, name } = event.target
+    setFormValues({...formValues, [name]:value})
   }
 
-  const submitForm = () => {
-    const newTeamMember = {
-      name: formValues.name.trim(),
+  const submit = (event) => {
+    event.preventDefault()
+    const newMembers = {
+      Name: formValues.Name.trim(),
+      Role: formValues.Role.trim(),
       email: formValues.email.trim(),
-      role: formValues.role
-    };
-    if (!newTeamMember.name || !newTeamMember.email || !newTeamMember.role) {
-      setError("You've gotta fill out all the fields");
-      return;
     }
-
-    axios.post('fakeapi.com', newTeamMember)
-      .then(res => {
-        setTeam([res.data, ...team]);
-        setFormValues(initialFormValues);
-        setError("");
-      })
-      .catch(err => console.error(err));
-
+    setTeam([...team, newMembers])
+    setFormValues(initialFormValues)
   }
-
-  useEffect(() => {
-    axios.get('fakeapi.com').then(res => setTeam(res.data))
-  }, [])
-
-
 
   return (
-    <div className='container'>
+    
+    <div className="form container">
       <h1>Team Builder</h1>
-      <h3 className="error">{error}</h3>
-      <TeamForm
-        // 🔥 STEP 2 - The form component needs its props.
-        //  Check implementation of FriendForm
-        //  to see what props it expects.
-        values={formValues}
-        update={updateForm}
-        submit={submitForm}
-      />
-      {/* 
-        const props = {values: formValues, update: updateForm, submit: submitForm}
-      
-        TeamForm(props) 
-      */}
-
-      {
-        team.map(member => {
-          return (
-            <Team key={member.id} details={member} />
-          )
-        })
-      }
-
-      
+        <div className="form container" >
+          {
+            team.map((member, idx) => {
+            return <div className='form-group inputs ' 
+            key={idx}>{member.Name} - {member.Role} - {member.email}</div>
+            })
+          }
+        </div>
+        
+          <form className="form container" onSubmit={submit}>
+            <label>Name
+              <input
+                type='text'
+                name='Name'
+                onChange={change}
+                value={formValues.Name}
+              />
+            </label>
+            <label> E-mail
+              <input
+                type='text'
+                name='Role'
+                onChange={change}
+                value={formValues.Role}
+              />
+            </label>
+            <label>Role
+              <select value={formValues.role} name="role" onChange={change}>
+            <option value=''>-- Select a Role --</option>
+            <option value='Student'>Student</option>
+            <option value='TL'>Team Lead</option>
+            <option value='Instructor'>Instructor</option>
+            <option value='Alumni'>Alumni</option>
+            <option value='Founder and CEO'></option>
+          </select>
+            </label>
+            <button>submit</button>
+          </form>
+        {/* </header>  */}
+         
     </div>
   )
+
 }
+
+export default App;
